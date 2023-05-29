1748d6a3544fe17fc0e2d7ac819c4cf2c2c05b09
marcoscabrera <marcoscabrera021@gmail.com>
2023-05-29 16:44:14 -0300

    Logica terminada

--- Logica/Program.cs
+++ Logica/Program.cs
@@ -7,113 +7,135 @@ namespace Logica
     {
         static void Main(string[] args)
         {
-            int opcion;
-            bool salir = false;
-            bool aniadir = false;
-            char seguir;
-
-            // Logica del programa
+    
             Vendedor vendedor = new Vendedor();
-            Producto producto = new Producto();
             Cliente cliente = new Cliente();
-            CompraCarrito compraCarrito = new CompraCarrito();
+            Cliente cliente2 = new Cliente();
 
-            while (!salir) 
+            Producto producto = new Producto();
+            Heladera heladera = new Heladera();
+            Carrito carrito = new Carrito();
+            Historial historial = new Historial();
+
+            Console.WriteLine("**************** SELECCION DE CLIENTE *******************");
+            cliente = vendedor.getCliente(3);
+            vendedor.getMontoCliente(cliente);
+            Console.WriteLine($"Cliente: {cliente.Nombre} {cliente.Apellido}, Saldo: {Vendedor.MontoCliene}");
+
+            //Console.WriteLine("***************** REPONER ******************");
+            //producto = vendedor.getProducto(0);
+            //Console.WriteLine($"Stock a Reponer: {producto.Stock}");
+            //vendedor.Reponer(producto, "5");
+            //Console.WriteLine($"Stock del producto: {producto.Stock}");
+
+            Console.WriteLine("[ VENDER ]");
+            producto = vendedor.getProducto(2);
+            vendedor.CargarCarrito(producto);
+            vendedor.CargarCarrito(producto);
+            producto = vendedor.getProducto(4);
+            vendedor.CargarCarrito(producto);
+            producto = vendedor.getProducto(5);
+            vendedor.CargarCarrito(producto);
+
+            Console.WriteLine("[ COMPRAR ]");
+            carrito.Comprar(vendedor.ListCarrito, Vendedor.MontoCliene, cliente, true);
+
+            Console.WriteLine("[ CARRITO ]: ");
+            foreach (Producto p in vendedor.ListCarrito)
             {
-                Console.WriteLine("ABM VENDEDOR");
-                Console.WriteLine("[1] Seleccionar cliente");
-                Console.WriteLine("[2] Reponer Stock");
-                Console.WriteLine("[3] Añadir al Carrito");
-                Console.WriteLine("[4] Ver Carrito");
-                Console.WriteLine("[5] Comprar");
-                Console.WriteLine("[6] Ver Factura");
-                Console.WriteLine("[7] ver Historial");
-                Console.WriteLine("[8] Ordenar Heladera");
+                Console.WriteLine($"{p.Nombre},{p.Stock},{p.Precio},{p.Tipo}");
+            }
 
-                Console.WriteLine("[9] Salir");
+            Console.WriteLine("\n[ LIMPIAR CARRITO ]");
+            vendedor.LimpiarCarrito();
 
-                Console.Write("Seleccione una opcion: ");
-                opcion = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\n[ FACTURA ]: {Factura.nFactura} ");
+            foreach (Factura p in carrito.ListaFactura)
+            {
+                Console.WriteLine($"{p.Nombre},{p.Cantidad},{p.PrecioUnitario},{p.Total}");
+            }
+            Console.WriteLine($"TOTAL: {carrito.Monto}");
 
-                Console.Clear();
-                
-                switch (opcion)
-                {
-                    case 1:                      
-                        // Selecciono cliente para vendedor
-                        cliente = vendedor.SeleccionDeCliente();
-                        vendedor.MostrarClienteSeleccionado(cliente);
-                        vendedor.CargarSalso(cliente);
-                        break;
-                    case 2:
-                        // muestro la heladera
-                        vendedor.MostrarHeladera();
-                        // Selecciono un producto para cambiar el stock
-                        producto = vendedor.SeleccionarProducto();
-                        // mostrar producto selecionado
-                        vendedor.MostrarProductoSeleccionado(producto);
+            Console.WriteLine("\n[ LIMPIAR FACTURA ]");
+            carrito.LimpiarFactura();
 
-                        Console.Write("Ingrese el stock:  ");
-                        string stock = Console.ReadLine();
-                        // reponer stock
-                        vendedor.ReponerStock(producto, stock);
-                        // mostrar producto selecionado
-                        vendedor.MostrarProductoSeleccionado(producto);
-                        // actualizar heladera
-                        vendedor.ActualizarHeladera(producto);
-                        // mostrar heladera
-                        vendedor.MostrarHeladera();
-                        break;
-                    case 3:
-                        if (vendedor.CargaDeCliente())
-                        {
-                            while ( !aniadir )
-                            {
-                                vendedor.MostrarHeladera();
-                                producto = vendedor.SeleccionarProducto();
+            Console.WriteLine("\n**************** SELECCION DE CLIENTE *******************");
+            cliente2 = vendedor.getCliente(4);
+            vendedor.getMontoCliente(cliente2);
+            Console.WriteLine($"Cliente: {cliente2.Nombre} {cliente2.Apellido}, Saldo: {Vendedor.MontoCliene}");
 
-                                vendedor.CargaCarrito(producto);
+            Console.WriteLine("[ VENDER ]");
+            producto = vendedor.getProducto(6);
+            vendedor.CargarCarrito(producto);
+            vendedor.CargarCarrito(producto);
+            producto = vendedor.getProducto(24);
+            vendedor.CargarCarrito(producto);
+            producto = vendedor.getProducto(19);
+            vendedor.CargarCarrito(producto);
 
-                                Console.Write("Seguir añadiendo productos al carrito (N/S): ");
-                                string auxSeguir = Console.ReadLine();
+            Console.WriteLine("[ COMPRAR ]");
+            carrito.Comprar(vendedor.ListCarrito, Vendedor.MontoCliene, cliente2, false);
 
-                                if (char.TryParse(auxSeguir, out seguir) && seguir == 'N')
-                                {
-                                    aniadir = true;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Cliente no seleccionado");
-                        }
-                        aniadir = false;
+            Console.WriteLine("[ CARRITO ]: ");
+            foreach (Producto p in vendedor.ListCarrito)
+            {
+                Console.WriteLine($"{p.Nombre},{p.Stock},{p.Precio},{p.Tipo}");
+            }
 
-                        break;
-                    case 4:
-                        vendedor.MostrarCarrito();
-                        break;
-                    case 5:
-                        compraCarrito.Comprar(Vendedor.ListCarrito, Vendedor.SaldoCliente, cliente);
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        break;
-                    case 9:
-                        salir = true;
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida. Intente nuevamente.");
-                        break;
-                }
+            Console.WriteLine("\n[ LIMPIAR CARRITO  ]");
+            vendedor.LimpiarCarrito();
 
-                Console.WriteLine();
+            Console.WriteLine($"\n[ FACTURA ]: {Factura.nFactura} ");
+            foreach (Factura p in carrito.ListaFactura)
+            {
+                Console.WriteLine($"{p.Nombre},{p.Cantidad},{p.PrecioUnitario},{p.Total}");
+            }
+            Console.WriteLine($"TOTAL: {carrito.Monto}");
 
-            } 
+            Console.WriteLine("\n[ LIMPIAR FACTURA ]");
+            carrito.LimpiarFactura();
+
+            Console.WriteLine("\n**************** SELECCION DE CLIENTE *******************");
+            Cliente cliente3 = new Cliente("Marcos", "Cabrera", 10000);
+            Console.WriteLine($"Cliente: {cliente3.Nombre} {cliente3.Apellido}, Saldo: {cliente3.Saldo}");  
+
+            Console.WriteLine("[ VENDER ]");
+            producto = cliente3.getProducto(2);
+            cliente3.CargarCarrito(producto);
+            cliente3.CargarCarrito(producto);
+            producto = cliente3.getProducto(4);
+            cliente3.CargarCarrito(producto);
+            producto = cliente3.getProducto(5);
+            cliente3.CargarCarrito(producto);
 
 
+            Console.WriteLine("[ COMPRAR ]");
+            carrito.Comprar(cliente3.ListCarrito, cliente3.Saldo, cliente3, false);
 
+            Console.WriteLine("[ CARRITO ]: ");
+            foreach (Producto p in vendedor.ListCarrito)
+            {
+                Console.WriteLine($"{p.Nombre},{p.Stock},{p.Precio},{p.Tipo}");
+            }
+
+            Console.WriteLine("\n[ LIMPIAR CARRITO ]");
+            vendedor.LimpiarCarrito();
+
+            Console.WriteLine($"\n[ FACTURA ]: {Factura.nFactura} ");
+            foreach (Factura p in carrito.ListaFactura)
+            {
+                Console.WriteLine($"{p.Nombre},{p.Cantidad},{p.PrecioUnitario},{p.Total}");
+            }
+            Console.WriteLine($"TOTAL: {carrito.Monto}");
+
+            Console.WriteLine("\n[ LIMPIAR FACTURA ]");
+            carrito.LimpiarFactura();
+
+            Console.WriteLine("\n[ HISTORIAL ]: ");
+            foreach (Historial h in Vendedor.ListaHistorial)
+            {
+                Console.WriteLine($"{h.NombreProducto},{h.Cantidad},{h.Total},{h.Nombre},{h.Apellido}");
+            }
 
         }
     }
